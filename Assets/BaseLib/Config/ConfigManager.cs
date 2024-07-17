f2d63ad944f8cd26c067fab922a78b329460edcc
speedz <292648912@qq.com>
2024-07-17 23:21:55 +0800

    monosingleton

--- Assets/BaseLib/Config/ConfigManager.cs
+++ Assets/BaseLib/Config/ConfigManager.cs
@@ -2,67 +2,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
-using Framework;
 
-/// <summary>
-/// 配置表管理
-/// </summary>
-public static class ConfigManager
+namespace Framework
 {
-    static Dictionary<Type, IConfig> mConfigs = new Dictionary<Type, IConfig>();
-    
-    public static T Get<T>() where T : ScriptableObject, IConfig, new()
+    /// <summary>
+    /// 配置表管理
+    /// </summary>
+    public static class ConfigManager
     {
-        var type = typeof(T);
+        static Dictionary<Type, IConfig> mConfigs = new Dictionary<Type, IConfig>();
 
-        if (mConfigs.TryGetValue(type, out var result))
+        public static T Get<T>() where T : ScriptableObject, IConfig, new()
         {
+            var type = typeof(T);
+
+            if (mConfigs.TryGetValue(type, out var result))
+            {
+                return (T)result;
+            }
+
+            result = Load<T>();
             return (T)result;
         }
 
-        result = Load<T>();
-        return (T)result;
-    }
-
-    public static T Load<T>() where T : ScriptableObject, IConfig, new()
-    {
-        var type = typeof(T);
-        if (mConfigs.TryGetValue(type, out var result))
+        public static T Load<T>() where T : ScriptableObject, IConfig, new()
         {
-            return (T)result;
+            var type = typeof(T);
+            if (mConfigs.TryGetValue(type, out var result))
+            {
+                return (T)result;
+            }
+
+            T obj = null;
+            obj = ScriptableObject.CreateInstance<T>();
+            var path = obj.Path;
+            obj = Resources.Load<T>(path);
+            if (obj)
+            {
+                mConfigs.Add(obj.GetType(), obj);
+            }
+
+            return obj;
         }
 
-        T obj = null;
-        obj = ScriptableObject.CreateInstance<T>();
-        var path = obj.Path;
-        obj = Resources.Load<T>(path);
-        if (obj)
+        public static void UnLoad<T>() where T : ScriptableObject, IConfig
         {
-            mConfigs.Add(obj.GetType(), obj);
+            IConfig result = null;
+            var type = typeof(T);
+            if (mConfigs.TryGetValue(type, out result))
+            {
+                mConfigs.Remove(type);
+                Resources.UnloadAsset((T)result);
+            }
         }
 
-        return obj;
-    }
-
-    public static void UnLoad<T>() where T: ScriptableObject,IConfig
-    {
-        IConfig result = null;
-        var type = typeof(T);
-        if (mConfigs.TryGetValue(type, out result))
+        public static void CleanAll()
         {
-            mConfigs.Remove(type);
-            Resources.UnloadAsset((T)result);
-        }
-    }
-    
-    public static void CleanAll()
-    {
-        foreach (var kvp in mConfigs)
-        {
-            var configObj = (UnityEngine.Object)kvp.Value;
-            Resources.UnloadAsset(configObj);
-        }
+            foreach (var kvp in mConfigs)
+            {
+                var configObj = (UnityEngine.Object)kvp.Value;
+                Resources.UnloadAsset(configObj);
+            }
 
-        mConfigs.Clear();
+            mConfigs.Clear();
+        }
     }
 }
---
+++ Assets/BaseLib/MonoSingleton.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Framework
+{
+    /// <summary>
+    /// Mono singleton Class. Extend this class to make singleton component.
+    /// Example: 
+    /// <code>
+    /// public class Foo : MonoSingleton<Foo>
+    /// </code>. To get the instance of Foo class, use <code>Foo.instance</code>
+    /// Override <code>Init()</code> method instead of using <code>Awake()</code>
+    /// from this class.
+    /// </summary>
+    /// 
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+    {
+        protected static T _Instance = null;
+        private static bool _IsApplicationQuiting = false;
+
+        public static T Instance
+        {
+            get
+            {
+                // Instance requiered for the first time, we look for it
+                if (_Instance == null && !_IsApplicationQuiting)
+                {
+                    _Instance = GameObject.FindObjectOfType(typeof(T)) as T;
+
+                    // Object not found, we create a temporary one
+                    if (_Instance == null)
+                    {
+                        _Instance = new GameObject(typeof(T).ToString(), typeof(T)).GetComponent<T>();
+
+                        // Problem during the creation, this should not happen
+                        if (!_Instance)
+                        {
+                            Debug.LogError("Problem during the creation of " + typeof(T).ToString());
+                        }
+                    }
+                }
+                return _Instance;
+            }
+            protected set
+            {
+                _Instance = value;
+            }
+        }
+
+        private bool _isInitialized = false;
+
+        public bool IsInitialized { get { return _isInitialized; } }
+
+        // If no other monobehaviour request the instance in an awake function
+        // executing before this one, no need to search the object.
+        private void Awake()
+        {
+            if (_Instance == null)
+            {
+                _Instance = this as T;
+            }
+            else if (_Instance != this)
+            {
+                Debug.LogError("Another instance of " + GetType() + " is already exist! Destroying self...");
+                DestroyImmediate(this);
+                return;
+            }
+
+            DontDestroyOnLoad(gameObject);
+            if (!IsInitialized)
+            {
+                Init();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (_Instance == this)
+            {
+                if (IsInitialized)
+                {
+                    Uninit();
+                }
+                _Instance = null;
+            }   
+        }
+
+        private void OnApplicationQuit()
+        {
+            _IsApplicationQuiting = true;
+        }
+
+        /// <summary>
+        /// This function is called when the instance is used the first time
+        /// Put all the initializations you need here, as you would do in Awake
+        /// </summary>
+        public virtual bool Init()
+        {
+            _isInitialized = true;
+            return true;
+        }       
+
+        public virtual void Uninit()
+        {
+            _isInitialized = false;
+        }
+
+    }
+
+}
